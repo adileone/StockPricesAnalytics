4b8c6c91c824c20694a329137e918cd66f912703
adileone <dileone.ale@gmail.com>
2019-04-30 17:16:30 +0200

    fix red

--- script/reducer.py
+++ script/reducer.py
@@ -3,6 +3,8 @@ import sys
 
 infile = sys.stdin
 
+myDict = {}
+
 #get input lines from stdin
 for line in infile:
     
@@ -15,8 +17,17 @@ for line in infile:
     try:
         a=float(words[1])
         b=float(words[2])
-        print "{} {}%".format(words[0], round(100*(b-a)/a, 2))
+        myDict[words[0]] = round(100*(b-a)/a, 2)
+        
     
     except ValueError as err:
         print(words, err)
+
+
+sorted_by_value = sorted(myDict.items(), key = lambda kv:(kv[1], kv[0]))
+sorted_by_value.reverse()
+for el in sorted_by_value[:10]:
+    print "{}\t{}%".format(el[0],el[1])
+
+
 
